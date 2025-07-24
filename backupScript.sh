27ec588f599a1b52fb3b537a0dc6f6b1c5d169b8
sahil <sahilsharmag007@gmail.com>
2025-07-24 20:55:51 +0530

    update backupScript.sh

--- backupScript.sh
+++ backupScript.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+Home="/home/sahil/devops"
+App_log="/home/sahil/devops/application.log"
+Sys_log="/home/sahil/devops/system.log"
+
 echo "### analysing logs"
 echo "=============="
-echo -e "\n### List of log files updated yersterday ###"
-find /Users/nana/logs -name "*.log" -mtime -1
+echo -e "\n### List of log files updated yesterday ###"
+find "$Home" -name "*.log" -mtime -1
 echo -e "\n### searching ERROR logs in application logs ###"
-grep "ERROR" /Users/nana/logs/application.log
+grep "ERROR" "$App_log"
 echo -e "\n### Number of errors found in application logs ###"
-grep -c "ERROR" /Users/nana/logs/application.log
+grep -c "ERROR" "$App_log"
 echo -e "\n### The last error from application logs ###"
-grep "ERROR" /Users/nana/logs/application.log | tail -1
+grep "ERROR" "$App_log" | tail -1
 echo -e "\n### The FATAL errors from system logs ###"
-grep "FATAL" /Users/nana/logs/system.log
+grep "FATAL" "$Sys_log"
 echo -e "\n### Number of FATAL errors in system logs ###"
-grep -c "FATAL" /Users/nana/logs/system.log
+grep -c "FATAL" "$Sys_log"
