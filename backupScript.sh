4eb54965dccec26cc7f2e4da90b12334db44fb34
sahil <sahilsharmag007@gmail.com>
2025-07-25 19:59:41 +0530

    update backupScript with forloop

--- backupScript.sh
+++ backupScript.sh
@@ -2,18 +2,26 @@
 Home="/home/sahil/devops"
 App_log="/home/sahil/devops/application.log"
 Sys_log="/home/sahil/devops/system.log"
-Errors=("ERROR" "FATAL""CRITICAL")
+Errors=("ERROR" "FATAL" "CRITICAL")
 echo "### analysing logs"
 echo "=============="
 echo -e "\n### List of log files updated yesterday ###"
-find "$Home" -name "*.log" -mtime -1
-echo -e "\n### searching "${Errors[0]}" logs in application logs ###"
-grep "${Errors[0]}" "$App_log"
-echo -e "\n### Number of errors found in application logs ###"
-grep -c "${Errors[0]}" "$App_log"
-echo -e "\n### The last error from application logs ###"
-grep "${Errors[0]}" "$App_log" | tail -1
-echo -e "\n### The "${Errors[1]}" errors from system logs ###"
-grep "${Errors[1]}" "$Sys_log"
-echo -e "\n### Number of "${Errors[1]}" errors in system logs ###"
-grep -c "${Errors[1]}" "$Sys_log"
+Log_Files=$(find "$Home" -name "*.log" -mtime -1)
+echo -e "\n### Total Errors:"
+echo "$Log_Files"
+for log_file in "$Log_Files"; do 
+    echo -e "\n==================================================="
+    echo "======================$log_file====================="
+    echo "========================================================"
+
+    echo -e "\n### searching "${Errors[0]}" logs in $log_file ###"
+    grep "${Errors[0]}" "$log_file"
+    echo -e "\n### Number of errors found in $log_file ###"
+    grep -c "${Errors[0]}" "$log_file"
+    echo -e "\n### The last error from $log_file ###"
+    grep "${Errors[0]}" "$log_file" | tail -1
+    echo -e "\n### The "${Errors[1]}" errors from $log_file ###"
+    grep "${Errors[1]}" "$log_file"
+    echo -e "\n### Number of "${Errors[1]}" errors in $log_file ###"
+    grep -c "${Errors[1]}" "$log_file"
+done
