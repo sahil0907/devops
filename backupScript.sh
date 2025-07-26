def9050bbc19102e9fd3ffef03c770faaf6710fc
sahil <sahilsharmag007@gmail.com>
2025-07-26 20:44:43 +0530

    update backupScript with advance syntax

--- backupScript.sh
+++ backupScript.sh
@@ -6,22 +6,18 @@ Errors=("ERROR" "FATAL" "CRITICAL")
 echo "### analysing logs"
 echo "=============="
 echo -e "\n### List of log files updated yesterday ###"
-Log_Files=$(find "$Home" -name "*.log" -mtime -1)
-echo -e "\n### Total Errors:"
+Log_Files=$(find "$Home" -name "*.log")
+echo -e "\n### Files:"
 echo "$Log_Files"
-for log_file in "$Log_Files"; do 
+for log_file in $Log_Files; do 
+    filename=$(basename "$log_file")
     echo -e "\n==================================================="
-    echo "======================$log_file====================="
+    echo "======================$filename====================="
     echo "========================================================"
-
-    echo -e "\n### searching "${Errors[0]}" logs in $log_file ###"
-    grep "${Errors[0]}" "$log_file"
-    echo -e "\n### Number of errors found in $log_file ###"
-    grep -c "${Errors[0]}" "$log_file"
-    echo -e "\n### The last error from $log_file ###"
-    grep "${Errors[0]}" "$log_file" | tail -1
-    echo -e "\n### The "${Errors[1]}" errors from $log_file ###"
-    grep "${Errors[1]}" "$log_file"
-    echo -e "\n### Number of "${Errors[1]}" errors in $log_file ###"
-    grep -c "${Errors[1]}" "$log_file"
+    for error in ${Errors[@]};do
+        echo -e "\n### searching "$error" logs in $log_file ###"
+        grep "$error" "$log_file"
+        echo -e "\n### Number of errors found in $log_file ###"
+        grep -c "$error" "$log_file"
+    done
 done
